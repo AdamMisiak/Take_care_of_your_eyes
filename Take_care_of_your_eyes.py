9ba508f6e5933fd01ea8b6cc13634e7442fbeeb4
Adam Misiak <adammisiak3@gmail.com>
2019-12-24 13:26:57 +0100

    Created micro script

---
+++ Take_care_of_your_eyes.py
@@ -0,0 +1,8 @@
+import tkinter
+import tkinter.messagebox
+
+window = tkinter.Tk()
+window.withdraw()
+warning = tkinter.messagebox.showwarning('Take care of your eyes!','Look out the window and blink several times!')
+
+
